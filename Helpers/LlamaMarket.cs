971c6ceee3fee6121b21f5982524e341ae058169
Catherine Renelle <32781029+catrenelle@users.noreply.github.com>
2023-06-11 10:20:04 -0400

    Fixing timing issues in ocean voyages.

---
+++ Helpers/LlamaMarket.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using ff14bot.Helpers;
+using ff14bot.Managers;
+
+namespace OceanTripPlanner.Helpers
+{
+	public static class LlamaMarket
+    {
+        private static Func<Task> _stopGently, _selfRepairWithMenderFallback;
+        private static Func<int> _getEmptyInventorySlotCount;
+        private static Action _openWindow;
+
+        private static object _llamamarket;
+        private static MethodInfo _orderMethod;
+
+        static LlamaMarket()
+        {
+            FindLlamaMarket();
+        }
+
+        public static async Task<bool> ExecuteOrders(string json)
+        {
+            if (_orderMethod == null) { return false; }
+
+            return await (Task<bool>)_orderMethod.Invoke(_llamamarket, new object[] { json, false });
+        }
+
+        private static object GetLlamaMarketBotObject()
+        {
+            var loader = BotManager.Bots
+                .FirstOrDefault(c => c.Name == "Llama Market");
+
+            if (loader == null) { return null; }
+
+            var lisbethObjectProperty = loader.GetType().GetProperty("LlamaMarket");
+            var lisbeth = lisbethObjectProperty?.GetValue(loader);
+
+            return lisbeth;
+        }
+
+        private static void FindLlamaMarket()
+        {
+            try
+            {
+                var llamamarket = GetLlamaMarketBotObject();
+                if (llamamarket == null) { return; }
+
+                var orderMethod = llamamarket.GetType().GetMethod("ExecuteOrders");
+                if (orderMethod == null) { return; }
+
+                _orderMethod = orderMethod;
+                _llamamarket = llamamarket;
+
+                var apiObject = llamamarket.GetType().GetProperty("Api")?.GetValue(llamamarket);
+
+                if (apiObject != null)
+                {
+                    _stopGently = (Func<Task>)Delegate.CreateDelegate(typeof(Func<Task>), apiObject, "StopGently");
+                    _openWindow = (System.Action)Delegate.CreateDelegate(typeof(System.Action), apiObject, "OpenWindow");
+                }
+
+                Logging.Write("LlamaMarket found.");
+            }
+            catch (Exception)
+            {
+                Logging.Write("LlamaMarket could not be found/hooked. LlamaMarket functionality will be disabled.");
+            }
+        }
+
+        public static async Task StopGently()
+        {
+            if (_stopGently == null) { return; }
+            await _stopGently();
+        }
+
+        public static void OpenWindow()
+        {
+            _openWindow();
+        }
+    }
+}
--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -32,28 +32,30 @@
             this.tabControl = new System.Windows.Forms.TabControl();
             this.Settings = new System.Windows.Forms.TabPage();
             this.routeInformationTab = new System.Windows.Forms.TabPage();
-            this.refreshButton = new System.Windows.Forms.Button();
-            this.toolsTab = new System.Windows.Forms.TabPage();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.refreshMissingFishButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.route1 = new System.Windows.Forms.TabPage();
-            this.route2 = new System.Windows.Forms.TabPage();
-            this.route3 = new System.Windows.Forms.TabPage();
-            this.label3 = new System.Windows.Forms.Label();
-            this.routeNameValue = new System.Windows.Forms.Label();
             this.scheduleGrid = new System.Windows.Forms.DataGridView();
             this.Day = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Route = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.RouteTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Objectives = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.routeNameValue = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.route1 = new System.Windows.Forms.TabPage();
+            this.route2 = new System.Windows.Forms.TabPage();
+            this.route3 = new System.Windows.Forms.TabPage();
+            this.toolsTab = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.refreshMissingFishButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl.SuspendLayout();
             this.Settings.SuspendLayout();
             this.routeInformationTab.SuspendLayout();
-            this.toolsTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.scheduleGrid)).BeginInit();
+            this.toolsTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // propertyGrid1
@@ -103,114 +105,6 @@
             this.routeInformationTab.TabIndex = 1;
             this.routeInformationTab.Text = "Schedule";
             this.routeInformationTab.UseVisualStyleBackColor = true;
-            // 
-            // refreshButton
-            // 
-            this.refreshButton.Location = new System.Drawing.Point(295, 6);
-            this.refreshButton.Name = "refreshButton";
-            this.refreshButton.Size = new System.Drawing.Size(75, 23);
-            this.refreshButton.TabIndex = 7;
-            this.refreshButton.Text = "Refresh";
-            this.refreshButton.UseVisualStyleBackColor = true;
-            this.refreshButton.Click += new System.EventHandler(this.refreshRouteInformation);
-            // 
-            // toolsTab
-            // 
-            this.toolsTab.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.toolsTab.Controls.Add(this.label2);
-            this.toolsTab.Controls.Add(this.label1);
-            this.toolsTab.Controls.Add(this.refreshMissingFishButton);
-            this.toolsTab.Controls.Add(this.button1);
-            this.toolsTab.Location = new System.Drawing.Point(4, 22);
-            this.toolsTab.Name = "toolsTab";
-            this.toolsTab.Size = new System.Drawing.Size(378, 450);
-            this.toolsTab.TabIndex = 2;
-            this.toolsTab.Text = "Tools";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(145, 115);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(190, 65);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Refresh the Missing Fish cache. This \r\ncan be useful if you caught ocean fish \r\no" +
-    "utside of using Ocean Trip. This will \r\nrequire you to restart the BotBase to\r\nt" +
-    "ake effect.";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 55);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 26);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Open Lisbeth BotBase Panel to manage \r\nrecipes";
-            // 
-            // refreshMissingFishButton
-            // 
-            this.refreshMissingFishButton.Location = new System.Drawing.Point(27, 115);
-            this.refreshMissingFishButton.Name = "refreshMissingFishButton";
-            this.refreshMissingFishButton.Size = new System.Drawing.Size(98, 25);
-            this.refreshMissingFishButton.TabIndex = 3;
-            this.refreshMissingFishButton.Text = "Refresh Cache";
-            this.refreshMissingFishButton.UseVisualStyleBackColor = true;
-            this.refreshMissingFishButton.Click += new System.EventHandler(this.refreshMissingFishCache_Button);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(27, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 25);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Lisbeth";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Lisbeth_Button);
-            // 
-            // route1
-            // 
-            this.route1.Location = new System.Drawing.Point(4, 22);
-            this.route1.Name = "route1";
-            this.route1.Size = new System.Drawing.Size(378, 450);
-            this.route1.TabIndex = 3;
-            this.route1.Text = "Route1";
-            this.route1.UseVisualStyleBackColor = true;
-            // 
-            // route2
-            // 
-            this.route2.Location = new System.Drawing.Point(4, 22);
-            this.route2.Name = "route2";
-            this.route2.Size = new System.Drawing.Size(378, 450);
-            this.route2.TabIndex = 4;
-            this.route2.Text = "Route 2";
-            this.route2.UseVisualStyleBackColor = true;
-            // 
-            // route3
-            // 
-            this.route3.Location = new System.Drawing.Point(4, 22);
-            this.route3.Name = "route3";
-            this.route3.Size = new System.Drawing.Size(378, 450);
-            this.route3.TabIndex = 5;
-            this.route3.Text = "Route 3";
-            this.route3.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 11);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 13);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "Route:";
-            // 
-            // routeNameValue
-            // 
-            this.routeNameValue.AutoSize = true;
-            this.routeNameValue.Location = new System.Drawing.Point(48, 11);
-            this.routeNameValue.Name = "routeNameValue";
-            this.routeNameValue.Size = new System.Drawing.Size(97, 13);
-            this.routeNameValue.TabIndex = 16;
-            this.routeNameValue.Text = "Route Name Value";
             // 
             // scheduleGrid
             // 
@@ -284,6 +178,136 @@
             this.Objectives.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             this.Objectives.Width = 111;
             // 
+            // routeNameValue
+            // 
+            this.routeNameValue.AutoSize = true;
+            this.routeNameValue.Location = new System.Drawing.Point(48, 11);
+            this.routeNameValue.Name = "routeNameValue";
+            this.routeNameValue.Size = new System.Drawing.Size(97, 13);
+            this.routeNameValue.TabIndex = 16;
+            this.routeNameValue.Text = "Route Name Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Route:";
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(295, 6);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 7;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshRouteInformation);
+            // 
+            // route1
+            // 
+            this.route1.Location = new System.Drawing.Point(4, 22);
+            this.route1.Name = "route1";
+            this.route1.Size = new System.Drawing.Size(378, 450);
+            this.route1.TabIndex = 3;
+            this.route1.Text = "Route1";
+            this.route1.UseVisualStyleBackColor = true;
+            // 
+            // route2
+            // 
+            this.route2.Location = new System.Drawing.Point(4, 22);
+            this.route2.Name = "route2";
+            this.route2.Size = new System.Drawing.Size(378, 450);
+            this.route2.TabIndex = 4;
+            this.route2.Text = "Route 2";
+            this.route2.UseVisualStyleBackColor = true;
+            // 
+            // route3
+            // 
+            this.route3.Location = new System.Drawing.Point(4, 22);
+            this.route3.Name = "route3";
+            this.route3.Size = new System.Drawing.Size(378, 450);
+            this.route3.TabIndex = 5;
+            this.route3.Text = "Route 3";
+            this.route3.UseVisualStyleBackColor = true;
+            // 
+            // toolsTab
+            // 
+            this.toolsTab.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.toolsTab.Controls.Add(this.label4);
+            this.toolsTab.Controls.Add(this.button2);
+            this.toolsTab.Controls.Add(this.label2);
+            this.toolsTab.Controls.Add(this.label1);
+            this.toolsTab.Controls.Add(this.refreshMissingFishButton);
+            this.toolsTab.Controls.Add(this.button1);
+            this.toolsTab.Location = new System.Drawing.Point(4, 22);
+            this.toolsTab.Name = "toolsTab";
+            this.toolsTab.Size = new System.Drawing.Size(378, 450);
+            this.toolsTab.TabIndex = 2;
+            this.toolsTab.Text = "Tools";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Open LlamaMarket BotBase Panel";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(27, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "LlamaMarket";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.LlamaMarket_Button);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 65);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Refresh the Missing Fish cache. This \r\ncan be useful if you caught ocean fish \r\no" +
+    "utside of using Ocean Trip. This will \r\nrequire you to restart the BotBase to\r\nt" +
+    "ake effect.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Open Lisbeth BotBase Panel to manage \r\nrecipes";
+            // 
+            // refreshMissingFishButton
+            // 
+            this.refreshMissingFishButton.Location = new System.Drawing.Point(27, 136);
+            this.refreshMissingFishButton.Name = "refreshMissingFishButton";
+            this.refreshMissingFishButton.Size = new System.Drawing.Size(98, 25);
+            this.refreshMissingFishButton.TabIndex = 7;
+            this.refreshMissingFishButton.Text = "Refresh Cache";
+            this.refreshMissingFishButton.UseVisualStyleBackColor = true;
+            this.refreshMissingFishButton.Click += new System.EventHandler(this.refreshMissingFishCache_Button);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Lisbeth";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Lisbeth_Button);
+            // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -298,9 +322,9 @@
             this.Settings.ResumeLayout(false);
             this.routeInformationTab.ResumeLayout(false);
             this.routeInformationTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleGrid)).EndInit();
             this.toolsTab.ResumeLayout(false);
             this.toolsTab.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.scheduleGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -328,5 +352,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Route;
         private System.Windows.Forms.DataGridViewTextBoxColumn RouteTime;
         private System.Windows.Forms.DataGridViewTextBoxColumn Objectives;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
     }
 }
